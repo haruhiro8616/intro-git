997cd271a218feb43845f34ec6095a592b2000ea
haruhiro8616 <haruhiro8616.p@gmail.com>
2019-10-03 22:49:27 +0900

    20191003
    Signed-off-by: haruhiro8616 <haruhiro8616.p@gmail.com>

--- ruby_projects/case.rb
+++ ruby_projects/case.rb
@@ -3,3 +3,28 @@
 # peridot : 8月
 # sapphire : 9月
 # それ以外 :データが登録されていません
+
+# if
+# stone = 'ruby'
+# if stone == 'ruby'
+#   puts '7月'
+# elsif stone == 'peridot'
+#   puts '8月'
+# elsif stone == 'sapphire'
+#   puts '9月'
+# else
+#   puts 'データが登録されていません'
+# end
+
+# case
+stone = 'peridot'
+case stone
+when 'ruby'
+  puts '7月'
+when 'peridot'
+  puts '8月'
+when 'sapphire'
+  puts '9月'
+else
+  puts 'データが登録されていません'
+end
